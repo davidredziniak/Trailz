094baa8829ed25ac3761312f5c6f08481da47fcd
davidredziniak <redziniakdavid@gmail.com>
2024-06-26 15:20:19 -0400

    Add functionality for user roles

--- lib/functions.php
+++ lib/functions.php
@@ -19,4 +19,6 @@ require(__DIR__ . "/flash_messages.php");
 require(__DIR__ . "/duplicate_user_details.php");
 //reset session
 require(__DIR__ . "/reset_session.php");
+
+require(__DIR__ . "/get_url.php");
 ?>
