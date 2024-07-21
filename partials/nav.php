319d8f1d8594c1abbfdead95a212dc0af44b01ef
davidredziniak <redziniakdavid@gmail.com>
2024-07-21 12:00:44 -0400

    Rename files

--- partials/nav.php
+++ partials/nav.php
@@ -32,7 +32,7 @@ session_start();
         <?php if (is_logged_in()) : ?>
             <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
             <li><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
-            <li><a href="<?php echo get_url('create_trail.php'); ?>">Submit Trail</a></li>
+            <li><a href="<?php echo get_url('submit_trail.php'); ?>">Submit Trail</a></li>
 
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
