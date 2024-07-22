644a845caa4585580e8e31f9d7a337b516e0388f
davidredziniak <redziniakdavid@gmail.com>
2024-07-22 14:11:27 -0400

    Start styling view trails page

--- public_html/Project/view_trails.php
+++ public_html/Project/view_trails.php
@@ -14,37 +14,35 @@ if (isset($_GET["find"])) {
     $type = se($_GET, "find", null, false);
 
     // Check if query params are set
-    switch($type){
+    switch ($type) {
         case "location":
-            if(!isset($_GET["lat"]) || !isset($_GET["long"]) || !isset($_GET["radius"])){
+            if (!isset($_GET["lat"]) || !isset($_GET["long"]) || !isset($_GET["radius"])) {
                 flash("Latitude, longtitude or radius is not set.", "danger");
                 $hasError = true;
             }
             break;
         case "other":
-            if(!isset($_GET["length"]) || !isset($_GET["difficulty"]) || !isset($_GET["country"])){
+            if (!isset($_GET["length"]) || !isset($_GET["difficulty"]) || !isset($_GET["country"])) {
                 flash("Length, country or difficulty is not set.", "danger");
                 $hasError = true;
-
             }
             break;
         default:
             flash("Search type is invalid.", "danger");
             $hasError = true;
-    } 
-    
+    }
+
     $limit = 10;
 
-    if (isset($_GET["limit"])){
+    if (isset($_GET["limit"])) {
         // Check if limit is valid
         $user_limit = se($_GET, "limit", null, false);
-        if(!empty($user_limit)){
+        if (!empty($user_limit)) {
             $user_limit = intval($user_limit);
-            if ($user_limit > 100 || $user_limit < 1){
+            if ($user_limit > 100 || $user_limit < 1) {
                 flash("Limit needs to be in a range 1 to 100.", "danger");
                 $hasError = true;
-            }
-            else{
+            } else {
                 $limit = $user_limit;
             }
         }
@@ -53,7 +51,7 @@ if (isset($_GET["find"])) {
     //$sortfield = se($_GET, "sort", null, false);
     //$sortby = se($_GET, "sortby", "ASC", false);
 
-    
+
 
     // Check if sortby is valid
     //if ($sortby != "ASC" && $sortby != "DESC"){
@@ -61,17 +59,17 @@ if (isset($_GET["find"])) {
     //    $hasError = true;
     //}
 
-    if($type === "location" && !$hasError){
+    if ($type === "location" && !$hasError) {
         $lat = se($_GET, "lat", null, false);
         $long = se($_GET, "long", null, false);
         $radius = se($_GET, "radius", null, false);
 
-        if(empty($lat) || empty($long)){
+        if (empty($lat) || empty($long)) {
             flash("Latitude and longitude must both be set.", "danger");
             $hasError = true;
         }
 
-        if(empty($radius)){
+        if (empty($radius)) {
             flash("Radius must be set.", "danger");
             $hasError = true;
         }
@@ -81,33 +79,32 @@ if (isset($_GET["find"])) {
         $long = floatval($long);
 
         // Check radius
-        if ($radius <= 0 || $radius > 100){
+        if ($radius <= 0 || $radius > 100) {
             flash("Radius must be greater than 0 and less than 100 miles.", "danger");
             $hasError = true;
         }
 
         // Check latitude
-        if (!is_valid_latitude($lat)){
+        if (!is_valid_latitude($lat)) {
             flash("Latitude must be valid. -90.00 to 90.00", "danger");
             $hasError = true;
         }
 
         // Check longtitude
-        if (!is_valid_longtitude($long)){
+        if (!is_valid_longtitude($long)) {
             flash("Longitude must be valid. -90.00 to 90.00", "danger");
             $hasError = true;
         }
 
-        if(!$hasError){
+        if (!$hasError) {
             $db = getDB();
             $stmt = $db->prepare("SELECT id, name, city, country, length, difficulty, (3959 * acos(cos(radians(:lat)) * cos(radians(ST_X(`coord`))) * cos( radians(ST_Y(`coord`)) - radians(:long)) + sin(radians(:lat)) * sin(radians(ST_X(`coord`))))) AS distance FROM Trails HAVING distance <= :distance ORDER BY distance LIMIT " . intval($limit) . ";");
             try {
                 $stmt->execute([":lat" => $lat, ":long" => $long, ":distance" => $radius]);
                 $r = $stmt->fetchAll();
-                if($r){
+                if ($r) {
                     $result = $r;
-                }
-                else{
+                } else {
                     flash("No results available.", "danger");
                 }
             } catch (Exception $e) {
@@ -115,8 +112,8 @@ if (isset($_GET["find"])) {
             }
         }
     }
-    
-    if($type === "other" && !$hasError){
+
+    if ($type === "other" && !$hasError) {
         $length = se($_GET, "length", "", false);
         $country = se($_GET, "country", "", false);
         $diff = se($_GET, "difficulty", "", false);
@@ -124,45 +121,42 @@ if (isset($_GET["find"])) {
         $search_country = false;
         $search_diff = false;
 
-        if (empty($length) && empty($country) && empty($diff)){
+        if (empty($length) && empty($country) && empty($diff)) {
             $hasError = true;
             flash("You must specify at least one field (length, country, or difficulty).");
         }
 
         // Check if length is provided 
-        if(!empty($length)){
+        if (!empty($length)) {
             $length = floatval($length);
-            if($length <= 0 || $length > 100){
+            if ($length <= 0 || $length > 100) {
                 $hasError = true;
                 flash("Length of the trails requested is invalid.", "danger");
-            }
-            else{
+            } else {
                 $search_length = true;
             }
         }
 
         // Check if country is provided
-        if (!empty($country)){
+        if (!empty($country)) {
             $country = trim($country);
-            if(strlen($country) <= 0 || strlen($country) > 30){
+            if (strlen($country) <= 0 || strlen($country) > 30) {
                 $hasError = true;
                 flash("Country requested is invalid.", "danger");
-            }
-            else{
+            } else {
                 $search_country = true;
             }
         }
 
         // Check if specific difficulty is provided
-        if (!empty($diff)){
+        if (!empty($diff)) {
             $diff = trim($diff);
-            if (!in_array($diff, $difficulties)){
+            if (!in_array($diff, $difficulties)) {
                 $hasError = true;
                 flash("Invalid difficulty requested.", "danger");
-            }
-            else{
+            } else {
                 $search_diff = true;
-                switch($diff){
+                switch ($diff) {
                     case "beg":
                         $diff = "Beginner";
                         break;
@@ -178,24 +172,24 @@ if (isset($_GET["find"])) {
                 }
             }
         }
-        
+
         // Build query
-        if(!$hasError){
+        if (!$hasError) {
             $query = "";
 
-            if($search_country){
+            if ($search_country) {
                 $query .= "country='" . $country . "'";
             }
 
-            if($search_diff){
-                if(strlen($query) > 0){
+            if ($search_diff) {
+                if (strlen($query) > 0) {
                     $query .= " AND ";
                 }
                 $query .= "difficulty='" . $diff . "'";
             }
 
-            if($search_length){
-                if(strlen($query) > 0){
+            if ($search_length) {
+                if (strlen($query) > 0) {
                     $query .= " AND ";
                 }
                 $query .= "length <=" . $length . "";
@@ -206,66 +200,82 @@ if (isset($_GET["find"])) {
             try {
                 $stmt->execute();
                 $r = $stmt->fetchAll();
-                if($r){
+                if ($r) {
                     $result = $r;
-                }
-                else{
+                } else {
                     flash("No results available.", "danger");
                 }
             } catch (Exception $e) {
                 flash(". var_export($e, true) .", "danger");
             }
         }
-
     }
 }
 ?>
-<form method="GET" onsubmit="return validate(this);">
-    <div class="mb-3">
-        <label for="lat">Latitude:</label>
-        <input type="text" name="lat" id="lat" />
-    </div>
-    <div class="mb-3">
-        <label for="long">Longitude:</label>
-        <input type="text" name="long" id="long" />
-    </div>
-    <div class="mb-3">
-        <label for="lat">Radius:</label>
-        <input type="radius" name="radius" id="radius" />
-    </div>
-    <div class="mb-3">
-        <label for="limit">Limit:</label>
-        <input type="number" name="limit" id="limit" />
-    </div>
-    <button class="btn" name="find" value="location" type="submit">Find</button>
-</form>
 
-<form method="GET" onsubmit="return validate(this);">
-    <div class="mb-3">
-        <label for="country">Country:</label>
-        <input type="text" name="country" id="country" />
+<body class="bg-dark">
+    <div class="container">
+        <div class="row gx-1">
+            <div class="col-md-6">
+                <div class="container-sm mt-5 p-5 rounded-2" style="background-color: #c5c5c5;">
+                    <h4>By Location</h4>
+                    <hr>
+                    <form method="GET" onsubmit="return validate(this);">
+                        <div class="mb-3">
+                            <label for="lat" class="form-label">Latitude:</label>
+                            <input type="text" name="lat" id="lat" class="form-control" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="long" class="form-label">Longitude:</label>
+                            <input type="text" name="long" id="long" class="form-control" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="lat" class="form-label">Radius:</label>
+                            <input type="radius" name="radius" id="radius" class="form-control" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="limit" class="form-label">Limit:</label>
+                            <input type="number" name="limit" id="limit" class="form-control" />
+                        </div>
+                        <button class="btn btn-primary" name="find" value="other" type="submit">Find</button>
+                    </form>
+                </div>
+
+            </div>
+            <div class="col-md-6">
+                <div class="container-sm mt-5 p-5 rounded-2" style="background-color: #c5c5c5;">
+                    <h4>By Specification</h4>
+                    <hr>
+                    <form method="GET" onsubmit="return validate(this);">
+                        <div class="mb-3">
+                            <label for="country" class="form-label">Country:</label>
+                            <input type="text" name="country" id="country" class="form-control" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="length" class="form-label">Maximum Length:</label>
+                            <input type="number" name="length" id="length" class="form-control" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="difficulty" class="form-label">Difficulty:</label>
+                            <select class="form-select" name="difficulty" id="difficulty">
+                                <option value="">Please choose</option>
+                                <option value="easy">Easiest</option>
+                                <option value="beg">Beginner</option>
+                                <option value="int">Intermediate</option>
+                                <option value="hard">Hard</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="limit" class="form-label">Limit:</label>
+                            <input type="number" name="limit" id="limit" class="form-control" />
+                        </div>
+                        <button class="btn btn-primary" name="find" value="other" type="submit">Find</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="mb-3">
-        <label for="length">Maximum Length:</label>
-        <input type="number" name="length" id="length" />
-    </div>
-    <div class="mb-3">
-        <label for="difficulty">Difficulty:</label>
-        <select name="difficulty" id="difficulty">
-            <option value="">Please choose</option>
-            <option value="easy">Easiest</option>
-            <option value="beg">Beginner</option>
-            <option value="int">Intermediate</option>
-            <option value="hard">Hard</option>
-        </select>
-        <br>
-    </div>
-    <div class="mb-3">
-        <label for="limit">Limit:</label>
-        <input type="number" name="limit" id="limit" />
-    </div>
-    <button class="btn" name="find" value="other" type="submit">Find</button>
-</form>
+</body>
 <?php if (!count($result) == 0) : ?>
     <div class="container-fluid">
         <h4>Trails</h4>
